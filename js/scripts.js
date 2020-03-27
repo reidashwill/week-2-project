10d05b33455ae094c48a4295f4d38ee756196b30
Reid Ashwill <reidashwill@gmail.com>
2020-03-27 10:05:24 -0700

    start testing limited functionality in submiting form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-
+  $("form#form").submit(function(event) {
+    event.preventDefault();
+    var question1 = $("input:radio[name=question1]:checked").val();
+    if(question1 === elegence) {
+      $("#ruby").show();
+    }
+  })
 });
