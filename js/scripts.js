9968e5e787005edfdf1bdf887766d94601ab1bc2
Reid Ashwill <reidashwill@gmail.com>
2020-03-27 11:39:31 -0700

    implement the use of the flag class to facilitate the removal of old results

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
   $("form#form").submit(function(event) {
-    event.preventDefault();
-    var question1 = $("input:radio[name=question1]:checked").val();
-    if(question1 === "elegence") {
-      $("#ruby").removeClass("hide");
-      $("#ruby").show();
-
-    }
     
+    var question1 = $("input:radio[name=question1]:checked").val();
+    var question2 = $("input:radio[name=question2]:checked").val();
+    var question3 = $("input:radio[name=question3]:checked").val();
+    var question4 = $("input:radio[name=question4]:checked").val();
+    var question5 = $("input:radio[name=question5]:checked").val();
+    if(question1 === "elegence" && question2 === "adventurous" && question5 === "epicodus"){
+      $(".flag").addClass("hide");
+      $("#ruby").removeClass("hide");
+    }else if(question1 === "brute" && question2 ===  "boring" && question3 === "desert"){
+      $(".flag").addClass("hide");
+      $("#python").removeClass("hide");
+    }
+    event.preventDefault();
   })
 });
