84eaf91b21715e6ad2657be45140064a90a58614
Reid Ashwill <reidashwill@gmail.com>
2020-03-27 10:35:52 -0700

    implemente removeClass on a specific id to simplify the code once it gets bigger

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
   $("form#form").submit(function(event) {
     event.preventDefault();
     var question1 = $("input:radio[name=question1]:checked").val();
-    if(question1 === elegence) {
+    if(question1 === "elegence") {
+      $("#ruby").removeClass("hide");
       $("#ruby").show();
+
     }
+    
   })
 });
